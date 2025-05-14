816f467fd7b2e0f669910f140f7702588ee5acf2
HarbansS <144715114+HarbansS@users.noreply.github.com>
2025-05-14 11:47:09 -0400

    Update 114-boolean-completed.py

--- Domain1/Student/114-boolean-completed.py
+++ Domain1/Student/114-boolean-completed.py
@@ -1,8 +1,13 @@
-char_life = true
+char_life = False
 score = 0
-if char_life == False:
+if char_life == True:
     print ("game over")
 else:
     print ("you are still in the game")    
 
-
+char_life = True
+score = 0
+if char_life == True:
+    print ("game over")
+else:
+    print ("you are still in the game") 
